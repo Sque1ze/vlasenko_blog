76f2484c5ce3c900a0b1d75d4f0a37c5bdc67f69
Andriy <andriy2005playgame@gmail.com>
2025-06-12 13:17:05 +0000

    Лабораторна 3. Контроллери, роути, шаблони

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\RestTestController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,4 +14,10 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+});
+
+Route::resource('rest', RestTestController::class)->names('restTest');
+
+Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
+    Route::resource('posts', PostController::class)->names('blog.posts');
 });
