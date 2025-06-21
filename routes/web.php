b9650d049ba84c90589840a5cba1061fcb68c0da
Andriy <andriy2005playgame@gmail.com>
2025-06-21 14:56:06 +0000

    Лабораторна 12. Черги (Queue, Jobs)

--- routes/web.php
+++ routes/web.php
@@ -19,15 +19,15 @@ Route::middleware([
 
 Route::resource('rest', RestTestController::class)->names('restTest');
 
-Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
-    Route::resource('posts', PostController::class)->names('blog.posts');
-});
 Route::group(['prefix' => 'digging_deeper'], function () {
 
     Route::get('collections', [DiggingDeeperController::class, 'collections'])
 
         ->name('digging_deeper.collections');
 
+});
+Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
+    Route::resource('posts', PostController::class)->names('blog.posts');
 });
 
 //Адмінка
